0c8d8cc4ba970e0d07c7f8fda40821b7d36f2d56
kamsi-nw <orecreates@gmail.com>
2025-04-30 11:27:29 +0100

    Fixing Build errors

--- supabase/functions/delete-cloudinary-images/index.ts
+++ supabase/functions/delete-cloudinary-images/index.ts
@@ -120,15 +120,3 @@ async function generateSignature(
   const hashArray = Array.from(new Uint8Array(hashBuffer))
   return hashArray.map(b => b.toString(16).padStart(2, '0')).join('')
 }
-
-/* To invoke locally:
-
-  1. Run `supabase start` (see: https://supabase.com/docs/reference/cli/supabase-start)
-  2. Make an HTTP request:
-
-  curl -i --location --request POST 'http://127.0.0.1:54321/functions/v1/delete-cloudinary-images' \
-    --header 'Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZS1kZW1vIiwicm9sZSI6ImFub24iLCJleHAiOjE5ODM4MTI5OTZ9.CRXP1A7WOeoJeXxjNni43kdQwgnWNReilDMblYTn_I0' \
-    --header 'Content-Type: application/json' \
-    --data '{"name":"Functions"}'
-
-*/
